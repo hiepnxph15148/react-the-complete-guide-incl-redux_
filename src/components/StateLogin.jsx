dd14a3563885d7d7b91535abb4b3bc0e7f8e0b90
hiepnxph <hiepnxph@gmail.com>
2025-03-07 00:59:08 +0700

    nhận all dữ liệu từ datafrom và validate dữ liệu khi nhập vào input

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,6 +8,11 @@ export default function Login() {
     email: "",
     password: "",
   });
+  const [didEdit,setDidEdit] = useState({
+    email:false,
+    password: false,
+  });
+  const emailIsValid = didEdit.email && !enteredValue.email.includes('@')
 
   function handleSubmit(event) {
     event.preventDefault(); // Prevent form submission
@@ -20,8 +25,19 @@ export default function Login() {
       ...prevValue,
       [identifier]: value, // Update the entered value for the specific identifier
     }));
-  }
+    // Update the entered value for the specific identifier
+    setDidEdit((prevEdit)=>({
+      ...prevEdit,
+      [identifier]: false, // Update the entered false for
+    }))
 
+  }
+  function handleInputBlur(identifier) {
+    setDidEdit((prevState) => ({
+     ...prevState,
+      [identifier]: true,
+    }));
+  }
   // function handleEmailChange(event) {
   //   setEnteredEmail(event.target.value);
   // }
@@ -42,11 +58,14 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={()=>handleInputBlur('email')}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValue.email}
           />
+          <div className="control-error">
+            {emailIsValid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
-
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
           <input
